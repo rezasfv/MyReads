b70a5a26f9cd609723546efec1f1810e9f3b5472
Seyedreza Safavi <Seyedrseyedreza.safavi@studenti.unipd.it>
2024-07-09 19:41:48 +0200

    add search and update style with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,78 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import ListBooks from './ListBooks';
 
-
-const books = [
-  {
-    "id": "book1",
-    "title": "To Kill a Mockingbird",
-    "author": "Harper Lee",
-    "isbn": "978-0-06-112008-4",
-    "coverURL": "http://localhost:5001/1.jpg",
-    "publicationDate": "1960-07-11"
-  },
-  {
-    "id": "book2",
-    "title": "1984",
-    "author": "George Orwell",
-    "isbn": "978-0-452-28423-4",
-    "coverURL": "http://localhost:5001/2.jpg",
-    "publicationDate": "1949-06-08"
-  },
-  {
-    "id": "book3",
-    "title": "Moby Dick",
-    "author": "Herman Melville",
-    "isbn": "978-0-14-243724-7",
-    "coverURL": "http://localhost:5001/3.jpg",
-    "publicationDate": "1851-10-18"
-  },
-  {
-    "id": "book4",
-    "title": "Pride and Prejudice",
-    "author": "Jane Austen",
-    "isbn": "978-0-19-953556-9",
-    "coverURL": "http://localhost:5001/4.jpg",
-    "publicationDate": "1813-01-28"
-  },
-  {
-    "id": "book5",
-    "title": "The Great Gatsby",
-    "author": "F. Scott Fitzgerald",
-    "isbn": "978-0-7432-7356-5",
-    "coverURL": "http://localhost:5001/5.jpg",
-    "publicationDate": "1925-04-10"
-  },
-  {
-    "id": "book6",
-    "title": "War and Peace",
-    "author": "Leo Tolstoy",
-    "isbn": "978-0-14-303999-0",
-    "coverURL": "http://localhost:5001/6.jpg",
-    "publicationDate": "1869-01-01"
-  },
-  {
-    "id": "book7",
-    "title": "The Catcher in the Rye",
-    "author": "J.D. Salinger",
-    "isbn": "978-0-316-76948-0",
-    "coverURL": "http://localhost:5001/7.jpg",
-    "publicationDate": "1951-07-16"
-  }
-];
-
-
-
-
 class App extends Component {
+  state = {
+    books: []
+  };
 
-  
-  render(){
-    return(
-      <div>
-        <ListBooks books={books}/>
+  componentDidMount() {
+    fetch('/books.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ books: data });
+      })
+      .catch(error => console.error('Error fetching the books:', error));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <ListBooks books={this.state.books} />
       </div>
-    )
+    );
   }
 }
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
 import './ListBooks.css';
+import PropTypes from 'prop-types';
 
 class ListBooks extends Component {
-  render() {
-    return (
-      <div>
-        <ol className='book-list'>
-          {this.props.books.map((book) => (
-            <li key={book.id} className='book-item'>
-              <img src={book.coverURL} alt={`${book.title} cover`} className='book-cover' />
-              <h2 className='book-title'>{book.title}</h2>
-              <p className='book-details'><strong>Author:</strong> {book.author}</p>
-              <p className='book-details'><strong>ISBN:</strong> {book.isbn}</p>
-              <p className='book-details'><strong>Publication Date:</strong> {book.publicationDate}</p>
-            </li>
-          ))}
-        </ol>
-      </div>
-    );
-  }
+
+    static propTypes = {
+        books: PropTypes.array.isRequired
+    }
+
+    state = {
+        query: ''
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            query: event.target.value
+        });
+    }
+
+    render() {
+        const { books } = this.props;
+        const { query } = this.state;
+
+        // Filter books based on query
+        const filteredBooks = query === ''
+            ?books
+            :books.filter(book =>
+                book.title.toLowerCase().includes(query.toLowerCase()) ||
+                book.author.toLowerCase().includes(query.toLowerCase()) ||
+                book.isbn.toLowerCase().includes(query.toLowerCase())
+        );
+
+        return (
+            <div className="container">
+                {/* Search bar */}
+                <div className="row my-4">
+                    <div className="col">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search books..."
+                            value={query}
+                            onChange={this.handleInputChange}
+                        />
+                    </div>
+                </div>
+
+                {/* Book list */}
+                <div className="row">
+                    {filteredBooks.map((book) => (
+                        <div key={book.id} className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4">
+                            <div className="card h-100">
+                                <img src={book.coverURL} alt={`${book.title} cover`} className="card-img-top book-cover" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{book.title}</h5>
+                                    <p className="card-text"><strong>Author:</strong> {book.author}</p>
+                                    <p className="card-text"><strong>ISBN:</strong> {book.isbn}</p>
+                                    <p className="card-text"><strong>Publication Date:</strong> {book.publicationDate}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ListBooks;
